face7f191416170d751c1022da11aabe8b10fc1b
toriqul07 <torikul170074@gmail.com>
2025-06-13 01:46:05 +0600

    Profile and dashboard related works Done

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  constructor(private apiService: ApiService) {}
+  user: any = null;
+  message: string = '';
 
+  ngOnInit(): void {
+    this.fetchUserInfo();
+  }
+
+  fetchUserInfo(): void {
+    this.apiService.getLoggedInUserInfo().subscribe({
+      next: (res) => {
+        this.user = res;
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message ||
+            error?.message ||
+            'Unable to Get Profile Info' + error
+        );
+      },
+    });
+  }
+  //SHOW ERROR
+  showMessage(message: string) {
+    this.message = message;
+    setTimeout(() => {
+      this.message = '';
+    }, 4000);
+  }
 }
