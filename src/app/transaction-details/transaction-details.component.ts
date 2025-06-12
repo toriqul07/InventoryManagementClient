e2adb1d395ca0b30d34a079fe415c682be3a2432
toriqul07 <torikul170074@gmail.com>
2025-06-13 00:46:10 +0600

    Transaction related works Done

--- src/app/transaction-details/transaction-details.component.ts
+++ src/app/transaction-details/transaction-details.component.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../service/api.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-transaction-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './transaction-details.component.html',
-  styleUrl: './transaction-details.component.css'
+  styleUrl: './transaction-details.component.css',
 })
-export class TransactionDetailsComponent {
+export class TransactionDetailsComponent implements OnInit {
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
+  transactionId: string | null = '';
+  transaction: any = null;
+  status: string = '';
+  message: string = '';
+
+  ngOnInit(): void {
+    //extract transaction id from routes
+    this.route.params.subscribe((params) => {
+      this.transactionId = params['transactionId'];
+      this.getTransactionDetails();
+    });
+  }
+
+  getTransactionDetails(): void {
+    if (this.transactionId) {
+      this.apiService.getTransactionById(this.transactionId).subscribe({
+        next: (transactionData: any) => {
+          if (transactionData.status === 200) {
+            this.transaction = transactionData.transaction;
+            this.status = this.transaction.status;
+          }
+        },
+        error: (error) => {
+          this.showMessage(
+            error?.error?.message ||
+              error?.message ||
+              'Unable to Get Transaction by id ' + error
+          );
+        },
+      });
+    }
+  }
+
+  //UPDATE STATUS
+  handleUpdateStatus(): void {
+    if (this.transactionId && this.status) {
+      this.apiService
+        .updateTransactionStatus(this.transactionId, this.status)
+        .subscribe({
+          next: (result) => {
+            this.router.navigate(['/transaction']);
+          },
+          error: (error) => {
+            this.showMessage(
+              error?.error?.message ||
+                error?.message ||
+                'Unable to Update a Transaction ' + error
+            );
+          },
+        });
+    }
+  }
+  //SHOW ERROR
+  showMessage(message: string) {
+    this.message = message;
+    setTimeout(() => {
+      this.message = '';
+    }, 4000);
+  }
 }
--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaginationComponent } from '../pagination/pagination.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../service/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-transaction',
   standalone: true,
-  imports: [],
+  imports: [PaginationComponent, FormsModule, CommonModule],
   templateUrl: './transaction.component.html',
-  styleUrl: './transaction.component.css'
+  styleUrl: './transaction.component.css',
 })
-export class TransactionComponent {
+export class TransactionComponent implements OnInit {
+  constructor(private apiService: ApiService, private router: Router) {}
 
+  transactions: any[] = [];
+  message: string = '';
+  searchInput: string = '';
+  valueToSearch: string = '';
+  currentPage: number = 1;
+  totalPages: number = 0;
+  itemsPerPage: number = 10;
+
+  ngOnInit(): void {
+    this.loadTransactions();
+  }
+  //FETCH Transactions
+  loadTransactions(): void {
+    this.apiService.getAllTransactions(this.valueToSearch).subscribe({
+      next: (res: any) => {
+        const transactions = res.transactions || [];
+
+        this.totalPages = Math.ceil(transactions.length / this.itemsPerPage);
+
+        this.transactions = transactions.slice(
+          (this.currentPage - 1) * this.itemsPerPage,
+          this.currentPage * this.itemsPerPage
+        );
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message ||
+            error?.message ||
+            'Unable to Get all Transactions ' + error
+        );
+      },
+    });
+  }
+  //HANDLE SERCH
+  handleSearch(): void {
+    this.currentPage = 1;
+    this.valueToSearch = this.searchInput;
+    this.loadTransactions();
+  }
+  //NAVIGATE TGO TRANSACTIONS DETAILS PAGE
+  navigateTOTransactionsDetailsPage(transactionId: string): void {
+    this.router.navigate([`/transaction/${transactionId}`]);
+  }
+  //HANDLE PAGE CHANGRTE. NAVIGATR TO NEXT< PREVIOUS OR SPECIFIC PAGE CHANGE
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.loadTransactions();
+  }
+  //SHOW ERROR
+  showMessage(message: string) {
+    this.message = message;
+    setTimeout(() => {
+      this.message = '';
+    }, 4000);
+  }
 }
