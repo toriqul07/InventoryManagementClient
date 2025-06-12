a9c39b51254f4becc10f6953a2cec04d437a1fb4
toriqul07 <torikul170074@gmail.com>
2025-06-12 23:18:22 +0600

    sell product Done

--- src/app/sell/sell.component.ts
+++ src/app/sell/sell.component.ts
@@ -1,12 +1,84 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-sell',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './sell.component.html',
-  styleUrl: './sell.component.css'
+  styleUrl: './sell.component.css',
 })
-export class SellComponent {
+export class SellComponent implements OnInit {
+  constructor(private apiService: ApiService) {}
 
+  products: any[] = [];
+  productId: string = '';
+  description: string = '';
+  quantity: string = '';
+  message: string = '';
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  fetchProducts(): void {
+    this.apiService.getAllProducts().subscribe({
+      next: (res: any) => {
+        if (res.status === 200) {
+          this.products = res.products;
+        }
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message ||
+            error?.message ||
+            'Unable to get Products' + error
+        );
+      },
+    });
+  }
+
+  //Handle form submission
+  handleSubmit(): void {
+    if (!this.productId || !this.quantity) {
+      this.showMessage('Please fill all fields');
+      return;
+    }
+    const body = {
+      productId: this.productId,
+      quantity: parseInt(this.quantity, 10),
+      description: this.description,
+    };
+
+    this.apiService.sellProduct(body).subscribe({
+      next: (res: any) => {
+        if (res.status === 200) {
+          this.showMessage(res.message);
+          this.resetForm();
+        }
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message ||
+            error?.message ||
+            'Unable to sell a product' + error
+        );
+      },
+    });
+  }
+
+  resetForm(): void {
+    this.productId = '';
+    this.description = '';
+    this.quantity = '';
+  }
+
+  showMessage(message: string) {
+    this.message = message;
+    setTimeout(() => {
+      this.message = '';
+    }, 4000);
+  }
 }
